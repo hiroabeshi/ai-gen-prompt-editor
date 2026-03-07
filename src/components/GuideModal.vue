ece5ca48648c3bf8cf0369b68fc736d65ce0efc0
hiroabeshi <256374532+hiroabeshi@users.noreply.github.com>
2026-03-07 12:38:55 +0900

    feat: add GuideModal component

--- src/components/GuideModal.vue
+++ src/components/GuideModal.vue
@@ -68,6 +68,7 @@
           <ul class="guide-patch-notes__list">
             <li><strong>ver 0.1:</strong> リリース</li>
             <li><strong>ver 0.2:</strong> ランダマイザ実装</li>
+            <li><strong>ver 0.3:</strong> カテゴリ強化、1.0.0データ移行実装</li>
           </ul>
         </div>
       </div>
